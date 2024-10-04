fff33de904e5f430fa362fab33716cae9680e7fd
marie-adele <quagliozzi.marie@gmail.com>
2024-10-04 12:47:25 +0200

    first commit

---
+++ Exos_01_02_03/main.js
@@ -0,0 +1,11 @@
+console.log("Exos_01_02_03")
+
+let nom = prompt("Entrez votre nom :");
+ 
+let prenom = prompt("Entrez votre prénom :");
+ 
+let message = "Bonjour " + nom + " " + prenom + "!";
+ 
+console.log(message);
+
+alert(message);
