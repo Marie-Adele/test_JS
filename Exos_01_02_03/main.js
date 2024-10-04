9aeffb3f455d72b3f91066b22f62a07246e6cbba
marie-adele <quagliozzi.marie@gmail.com>
2024-10-04 13:49:07 +0200

    C04

--- Exos_01_02_03/main.js
+++ Exos_01_02_03/main.js
@@ -1,30 +1,23 @@
-console.log("Exos_01_02_03")
+console.log("Exos_01_02_03");
 
 let nom = prompt("Entrez votre nom :");
- 
 let prenom = prompt("Entrez votre prénom :");
- 
 let message01 = "Bonjour " + nom + " " + prenom + "!";
- 
-console.log(message);
-alert(message);
+console.log(message01);
+alert(message01);
 
 let nb1 = parseFloat(prompt("Veuillez entrer un premier nombre :"));
-
 let nb2 = parseFloat(prompt("Veuillez entrer un second nombre :"));
-
 let resultat = nb1 + nb2;
-
 console.log(nb1 + " + " + nb2 + " = " + resultat);
 alert(nb1 + " + " + nb2 + " = " + resultat);
 
-
 let rayon, hauteur, volume;
-rayon=Number(prompt("Saisir le rayon : "))
-hauteur=Number(prompt("Saisir la hauteur : "))
-volume=(Math.PI * rayon**2 * hauteur) / 3
+rayon=parseFloat(prompt("Saisir le rayon : "));
+hauteur=parseFloat(prompt("Saisir la hauteur : "));
+volume=(Math.PI * rayon**2 * hauteur);
 let message02 = "Le volume est de : " + volume.toFixed(2);
-console. log(message);
-alert.message;
+console.log(message02);
+alert(message02);
 
 
