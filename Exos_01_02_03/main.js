a152510c5983927908c60dee0f07ef9f8269bae7
marie-adele <quagliozzi.marie@gmail.com>
2024-10-04 13:42:05 +0200

    C03

--- Exos_01_02_03/main.js
+++ Exos_01_02_03/main.js
@@ -4,7 +4,7 @@ let nom = prompt("Entrez votre nom :");
  
 let prenom = prompt("Entrez votre prénom :");
  
-let message = "Bonjour " + nom + " " + prenom + "!";
+let message01 = "Bonjour " + nom + " " + prenom + "!";
  
 console.log(message);
 alert(message);
@@ -17,4 +17,14 @@ let resultat = nb1 + nb2;
 
 console.log(nb1 + " + " + nb2 + " = " + resultat);
 alert(nb1 + " + " + nb2 + " = " + resultat);
+
+
+let rayon, hauteur, volume;
+rayon=Number(prompt("Saisir le rayon : "))
+hauteur=Number(prompt("Saisir la hauteur : "))
+volume=(Math.PI * rayon**2 * hauteur) / 3
+let message02 = "Le volume est de : " + volume.toFixed(2);
+console. log(message);
+alert.message;
+
 
