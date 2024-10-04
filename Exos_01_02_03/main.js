797d9507aedbf9f38072c69c5c948cd84b9875c5
marie-adele <quagliozzi.marie@gmail.com>
2024-10-04 13:05:51 +0200

    C02

--- Exos_01_02_03/main.js
+++ Exos_01_02_03/main.js
@@ -7,5 +7,14 @@ let prenom = prompt("Entrez votre prénom :");
 let message = "Bonjour " + nom + " " + prenom + "!";
  
 console.log(message);
-
 alert(message);
+
+let nb1 = parseFloat(prompt("Veuillez entrer un premier nombre :"));
+
+let nb2 = parseFloat(prompt("Veuillez entrer un second nombre :"));
+
+let resultat = nb1 + nb2;
+
+console.log(nb1 + " + " + nb2 + " = " + resultat);
+alert(nb1 + " + " + nb2 + " = " + resultat);
+
